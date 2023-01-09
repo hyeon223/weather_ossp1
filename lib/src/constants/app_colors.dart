c1edc2793a747b68e9b3a8970455771ae26bf992
hyeon223 <wkdtmdgus0@gmail.com>
2023-01-09 18:18:36 +0900

    change first commit

---
+++ lib/src/constants/app_colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class AppColors {
+  static const rainBlueLight = Color(0xFF87CEEB);
+  static const rainBlueDark = Color(0xFF4682B4);
+  static const rainGradient = [rainBlueLight, rainBlueDark]; // 백그라운드 색상
+  static const accentColor = Color(0xFFE0FFFF); //Color(0xFFe96e50);
+}
---
+++ lib/src/features/weather_page/weather_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:open_weather_example_flutter/src/constants/app_colors.dart';
+import 'package:open_weather_example_flutter/src/features/weather_page/city_search_box.dart';
+import 'package:open_weather_example_flutter/src/features/weather_page/current_weather.dart';
+import 'package:open_weather_example_flutter/src/features/weather_page/hourly_weather.dart';
+
+class WeatherPage extends StatelessWidget {
+  const WeatherPage({Key? key, required this.city}) : super(key: key);
+  final String city;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: AppColors.rainGradient,
+          ),
+        ),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children:  [
+              Spacer(),
+              Text('지역을 검색하십시오',style: TextStyle(fontSize: 40)), // 검색할 도시
+              Spacer(), // 배치
+              CitySearchBox(),
+              Spacer(),
+              CurrentWeather(),
+              Spacer(),
+              ElevatedButton(onPressed: (){}, child: Text('추천')), // 1. 정보를 얻을 수 있는 버튼
+              Spacer(),
+              HourlyWeather(),
+              Spacer(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
